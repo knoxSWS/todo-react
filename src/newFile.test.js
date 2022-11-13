795d88db780f9a6a8eff130b4da2dde8915f9d34
Rob Knox <robert.knox@southernwater.co.uk>
2022-11-13 17:20:49 +0000

    added newFile(s)

--- src/newFile.test.js
+++ src/newFile.test.js
@@ -1,5 +1,5 @@
-//import { sort } from "./newFile"; 
+import { sort } from "./newFile"; 
 
 it('bubble sort', () => {
-  //expect(sort([0, 1, 2, 4])).toEqual([0, 1, 2, 4]);
+  expect(sort([0, 1, 2, 4])).toEqual([0, 1, 2, 4]);
 });
